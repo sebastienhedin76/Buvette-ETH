96abe30427bcf2f4e9b431cc196588b76b06d6a9
sebastienhedin76 <sebastien.hedin76@gmail.com>
2026-08-20 15:36:06 +0200

    Update config.js

--- Buvette-ETH-MVP/config.js
+++ Buvette-ETH-MVP/config.js
@@ -1,5 +1,5 @@
 // Copier ce fichier sous le nom config.js, puis renseigner les valeurs Supabase.
 window.BUVETTE_CONFIG = {
-  SUPABASE_URL: "https://BuvetteETH.supabase.co",
+  SUPABASE_URL: "https://ipzppvirgftmfncescla.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwenBwdmlyZ2Z0bWZuY2VzY2xhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcyMTE5NzUsImV4cCI6MjEwMjc4Nzk3NX0.xiWT7_jZzNHAfXOvDmpqKJE98jL-GCI9HIyksX_aIj4"
 };
